c9b0ffe6652baf5975c73295b0a170778383aa54
sladelaroy <fuunshofaleye1@gmail.com>
2025-02-19 07:05:55 +0100

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <a href="">
+          <Image src={assets.logo} className="w-28 cursor-pointer mr-14" />
+        </a>
+
+        <ul>
+          <li><a href="#top">Home</a></li>
+          <li><a href="#about">About Me</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#work">My Work</a></li>
+          <li><a href="#contact">Contact me</a></li>
+        </ul>
+
+        <div>
+          <a href="#contact">Contact <Image src={assets.arrow_icon} className="w-3"/></a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
