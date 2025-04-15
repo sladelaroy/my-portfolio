30452c249cf19d87afdfce37fd6a99241d88d68e
sladelaroy <fuunshofaleye1@gmail.com>
2025-04-15 10:10:35 +0100

    updated

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
         transition={{ duration: 0.6, delay: 0.7 }}
         className="max-w-2xl mx-auto font-Ovo "
       >
-        I'm a fullstack developer proficeint in building websites
+        a frontend/fullstack developer with a passion for creating pixel-perfect, responive and optimized websites/apps
       </motion.p>
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
         <motion.a
@@ -55,7 +55,7 @@ const Header = () => {
           initial={{ y: -30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="/My_Resume.pdf"
+          href="/Funsho_Faleye_Resume.docx"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-darkHover"
         >
